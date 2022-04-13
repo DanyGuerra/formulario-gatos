55eb7f780e156bf4367b509774119c4cfa485973
DanyGuerra <luis_daniel.grra15@hotmail.com>
2022-04-13 14:01:48 -0500

    Estilos en administrador

--- client/src/components/Administrador.jsx
+++ client/src/components/Administrador.jsx
@@ -2,112 +2,73 @@ import { useEffect, useState } from "react";
 import host from "../const";
 import styled from "styled-components";
 import IconoVideo from "./IconoVideo";
+import Header from "./Header";
 
 const Template = styled.div`
-  display: grid;
+  margin-top: 80px;
+  display: flex;
   width: 100%;
-  height: 100vh;
   row-gap: 10px;
   column-gap: 40px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  font-family: "Sen";
 
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(7, 1fr);
-
-  header {
-    grid-column-start: 1;
-    grid-column-end: 6;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    background: #aa9ceb;
-  }
-  .l-side {
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 2;
-    grid-row-end: 6;
-  }
-  .r-side {
-    grid-column-start: 5;
-    grid-column-end: 6;
-    grid-row-start: 2;
-    grid-row-end: 6;
-  }
-  .title-1 {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 3;
-    grid-row-end: 4;
-    background: #ffffff;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  }
-  .title-2 {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 4;
-    grid-row-end: 5;
-    /* background: #ffffff;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
-  }
-  .download-excel {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 5;
-    grid-row-end: 6;
-    display: flex;
-    flex-direction: column-reverse;
-
-    .btn-excel {
-      height: 66px;
-      border: 1px solid #000000;
-      border-radius: 10px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      #link-excel {
-        text-decoration: none;
-        color: black;
-      }
-    }
+  .title {
+    width: 60%;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
   }
   .filters {
-    grid-column-start: 3;
-    grid-column-end: 4;
-    grid-row-start: 2;
-    grid-row-end: 3;
-    button {
-      margin-right: 20px;
-      background-color: white;
-      border: 1px solid #000000;
-      box-sizing: border-box;
-      border-radius: 10px;
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+    .day {
+      .active {
+        color: white;
+        width: 80px;
+        height: 40px;
+        background: #260f5d;
+        border-radius: 4px;
+      }
+      button {
+        color: black;
+        width: 80px;
+        height: 40px;
+        background: white;
+        border-radius: 4px;
+        border: none;
+      }
     }
-  }
-  .encuestas {
-    grid-column-start: 3;
-    grid-column-end: 4;
-    grid-row-start: 3;
-    grid-row-end: 6;
-  }
-  .pagination {
-    grid-column-start: 3;
-    grid-column-end: 4;
-    grid-row-start: 6;
-    grid-row-end: 7;
-  }
-  footer {
-    grid-column-start: 1;
-    grid-column-end: 5;
-    grid-row-start: 7;
-    grid-row-end: 8;
+
+    .download-excel {
+      width: 160px;
+      height: 40px;
+      border: 1px solid #aa9ceb;
+      box-sizing: border-box;
+      border-radius: 4px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      a {
+        font-family: "Inter";
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        color: #aa9ceb;
+        text-decoration: none;
+      }
+    }
   }
 `;
 
 const Encuestas = styled.div`
   display: flex;
   flex-direction: column;
-  width: 500px;
-  height: 400px;
+  width: 60%;
+  height: 60vh;
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   overflow-y: scroll;
@@ -128,9 +89,9 @@ const Encuestas = styled.div`
     padding: 10px;
     width: 100%;
     height: 60px;
-    border-bottom: solid black 1px;
+    border-bottom: 1px solid #dee2e6;
     display: grid;
-    grid-template-columns: 20% 20% 60%;
+    grid-template-columns: 40% 20% 40%;
 
     .item {
       .icon {
@@ -151,7 +112,7 @@ const Encuestas = styled.div`
 
 const Administrador = () => {
   const [encuestas, setEncuestas] = useState([]);
-  const [day, setDay] = useState(1);
+  const [day, setDay] = useState("1");
 
   useEffect(() => {
     getAllEncuestas();
@@ -180,57 +141,70 @@ const Administrador = () => {
   };
 
   return (
-    <Template>
-      <header></header>
-      <div className="filters">
-        <button value={1} onClick={handleDayClick}>
-          Dia 1
-        </button>
-        <button value={2} onClick={handleDayClick}>
-          Dia 2
-        </button>
-      </div>
-      <div className="l-side"></div>
-      <div className="title-1">Numero total de encuestas</div>
-      <div className="title-2"></div>
-      <Encuestas className="encuestas">
-        <div className="row header">
-          <div className="item">Usuario</div>
-          <div className="item">Encuesta</div>
-          <div className="item">Video</div>
+    <>
+      <Header></Header>
+      <Template>
+        <div className="title">
+          <h1> Administrador</h1>
+        </div>
+        <div className="filters">
+          <div className="day">
+            <button
+              className={`${day === "1" ? "active" : ""}`}
+              value={1}
+              onClick={handleDayClick}
+            >
+              Dia 1
+            </button>
+            <button
+              className={`${day === "2" ? "active" : ""}`}
+              value={2}
+              onClick={handleDayClick}
+            >
+              Dia 2
+            </button>
+          </div>
+
+          <div className="download-excel">
+            <div className="btn-excel">
+              <a
+                id="link-excel"
+                href={`${host.HOST}admin/descargar-excel/${day}`}
+              >
+                Descargar excel
+              </a>
+            </div>
+          </div>
         </div>
 
-        {encuestas.map((item, index) => {
-          return (
-            <>
-              <div key={item.user} className="row">
-                <div className="item">{item.user}</div>
-                <div className="item">{item.day}</div>
-                <div className="item">
-                  <div className="icon">
-                    <a href={`${host.HOST}download/${item.videoInfo.Key}`}>
-                      <IconoVideo></IconoVideo>
-                    </a>
+        <Encuestas className="encuestas">
+          <div className="row header">
+            <div className="item">Usuario</div>
+            <div className="item">Encuesta</div>
+            <div className="item">Video</div>
+          </div>
+
+          {encuestas.map((item, index) => {
+            return (
+              <>
+                <div key={item.user} className="row">
+                  <div className="item">{item.user}</div>
+                  <div className="item">{item.day}</div>
+                  <div className="item">
+                    <div className="icon">
+                      <a href={`${host.HOST}download/${item.videoInfo.Key}`}>
+                        <IconoVideo></IconoVideo>
+                      </a>
+                    </div>
+                    <span>{item.videoInfo.Key}</span>
                   </div>
-                  <span>{item.videoInfo.Key}</span>
                 </div>
-              </div>
-            </>
-          );
-        })}
-      </Encuestas>
-
-      <div className="r-side"></div>
-      <div className="pagination"></div>
-      <div className="download-excel">
-        <div className="btn-excel">
-          <a id="link-excel" href={`${host.HOST}admin/descargar-excel/${day}`}>
-            Descargar excel
-          </a>
-        </div>
-      </div>
-      <footer></footer>
-    </Template>
+              </>
+            );
+          })}
+        </Encuestas>
+      </Template>
+    </>
   );
 };
 
